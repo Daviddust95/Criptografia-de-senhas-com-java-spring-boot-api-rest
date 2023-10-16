89578418e494e74891694038d659de6f529933d1
Alisson David de Melo <daviddust4@hotmail.com>
2023-10-16 00:56:42 -0300

    up

--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/repository/UserRepository.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/repository/UserRepository.java
@@ -2,11 +2,9 @@ package criptografia.senhas.CriptografiaDeSenhas.repository;
 
 import criptografia.senhas.CriptografiaDeSenhas.UsuarioModelo.Modelo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.ui.Model;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Modelo, Integer> {
 
-    public Optional<Model> findByLogin(String login);
+    public Optional<Modelo> findByLogin(String login);
 }
