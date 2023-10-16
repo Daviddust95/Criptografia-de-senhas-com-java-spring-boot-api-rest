ddb2abc72c8eecfb45a0ec343224ff0fa42ee4ad
Alisson David de Melo <daviddust4@hotmail.com>
2023-10-16 07:20:57 -0300

    Update UsuarioController.java

--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/controller/UsuarioController.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/controller/UsuarioController.java
@@ -1,16 +1,30 @@
 package criptografia.senhas.CriptografiaDeSenhas.controller;
 
+import criptografia.senhas.CriptografiaDeSenhas.UsuarioModelo.Modelo;
+import criptografia.senhas.CriptografiaDeSenhas.repository.UserRepository;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/usuario")
 public class UsuarioController {
 
 
+    private UserRepository repository;
+        this.repository = repository;
+        }
 
 
-    public 
+
+        @GetMapping(/"listarTodos")
+    public ResponseEntity<List<Modelo>> listarTodos() {
+
+        return ResponseEntity.ok(repository.findAll());
 
 
 
