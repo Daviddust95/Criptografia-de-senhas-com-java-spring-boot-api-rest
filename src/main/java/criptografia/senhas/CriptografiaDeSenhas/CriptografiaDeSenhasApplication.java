17268006ad7b4e040d5fec9249d8df0ef8f16a48
Alisson David de Melo <daviddust4@hotmail.com>
2023-10-15 23:14:16 -0300

    up

---
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/CriptografiaDeSenhasApplication.java
@@ -0,0 +1,13 @@
+package criptografia.senhas.CriptografiaDeSenhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptografiaDeSenhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptografiaDeSenhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/criptografia/senhas/CriptografiaDeSenhas/CriptografiaDeSenhasApplicationTests.java
@@ -0,0 +1,13 @@
+package criptografia.senhas.CriptografiaDeSenhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografiaDeSenhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
