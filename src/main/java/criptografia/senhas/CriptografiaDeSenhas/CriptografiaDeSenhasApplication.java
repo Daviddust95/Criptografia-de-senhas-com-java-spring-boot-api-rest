1cec5ff097f619cc4427288a08517b038a007b5b
Alisson David de Melo <daviddust4@hotmail.com>
2023-10-18 22:33:03 -0300

    Tirando a validação de usuário e senha
    A partir deste momento, não é mais necessário validar usuário e senha no localhost para logar na aplicação.

--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/CriptografiaDeSenhasApplication.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/CriptografiaDeSenhasApplication.java
@@ -2,8 +2,9 @@ package criptografia.senhas.CriptografiaDeSenhas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografiaDeSenhasApplication {
 
 	public static void main(String[] args) {
