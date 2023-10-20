11c327c16ec67fb9d16d415a901dd5ae7c610770
Alisson David de Melo <daviddust4@hotmail.com>
2023-10-19 23:45:28 -0300

    verificando os encoders
    primeiro, eu consulto o usuário
    se o usuário não for encontrado pelo login, é retornada a resposa não autorizado, agora, existe um boolean para verificar se o password é falso ou não.
    
    E necessário salvar o usuário e verificar se a senha está dando certo, se estiver correta, será confirmada, e, caso contrário não será autorizado o acesso.
    
    usa-se o método match para comparar a senha aberta e a senha encriptada.

--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/controller/UsuarioController.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/controller/UsuarioController.java
@@ -2,11 +2,13 @@ package criptografia.senhas.CriptografiaDeSenhas.controller;
 
 import criptografia.senhas.CriptografiaDeSenhas.UsuarioModelo.Modelo;
 import criptografia.senhas.CriptografiaDeSenhas.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuario")
@@ -30,4 +32,24 @@ public class UsuarioController {
         usuario.setPassword(encoder.encode(usuario.getPassword()));
         return ResponseEntity.ok(repository.save(usuario));
     }
+    @GetMapping("/validarSenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,
+                                                @RequestParam String password) {
+
+    Optional<Modelo> optUsuario = repository.findByLogin(login);
+    if (optUsuario.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+    }
+    
+
+    Modelo usuario = optUsuario.get();
+    boolean valid = encoder.matches(password, usuario.getPassword());
+
+    HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
+
+
+
+    }
+
 }
