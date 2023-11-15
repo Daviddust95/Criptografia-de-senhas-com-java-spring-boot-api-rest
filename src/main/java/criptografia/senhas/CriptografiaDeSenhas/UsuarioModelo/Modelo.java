484ab5b9d8a48382f4aa9962387fa4c1abd97510
Alisson David de Melo <daviddust4@hotmail.com>
2023-11-15 09:54:17 -0300

    Criando comentários dentro do projeto
    Criando comentários para tornar o código mais didático e explicativo.

--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/UsuarioModelo/Modelo.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/UsuarioModelo/Modelo.java
@@ -1,3 +1,21 @@
+/*Este é o usuário modelo da aplicação
+Ele pertecence ao tipo de dados, essa
+classe de usuário possui dois campos do
+tipo String e métodos get, set e hashCode.
+
+Tipo de Dados: String é um tipo de dado em
+muitas linguagens de programação, incluindo
+Java. Ele é usado para armazenar sequências de
+caracteres, como palavras ou frases.
+
+Métodos Get e Set: Getters: São métodos usados para
+obter o valor de um campo.
+
+Setters: São métodos usados para definir o valor de um campo
+
+hashCode: Este é um método em Java que retorna um valor
+numérico associado a um objeto.
+*/
 package criptografia.senhas.CriptografiaDeSenhas.UsuarioModelo;
 
 
@@ -14,8 +32,11 @@ import lombok.NoArgsConstructor;
 public class Modelo {
 
     @Id
+    //Criando atribuição de id automática
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Criando chave primária
     private Integer id;
+    //Atribuição para que o login nunca se repita
     @Column (unique = true)
     private String login;
     private String password;
--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/repository/UserRepository.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/repository/UserRepository.java
@@ -4,7 +4,12 @@ import criptografia.senhas.CriptografiaDeSenhas.UsuarioModelo.Modelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Modelo, Integer> {
 
+/*Explicando qual o  meu modelo de dados
+e qual o tipo  do meu id*/
+
+public interface UserRepository extends JpaRepository<Modelo, Integer> {
+/*Fazer a busca do usuário no banco
+de dados utilizando o campo login */
     public Optional<Modelo> findByLogin(String login);
 }
