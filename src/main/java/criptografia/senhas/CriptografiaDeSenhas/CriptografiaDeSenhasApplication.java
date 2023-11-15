3b238052d873c0ab67d0f6c4bda490f38b7e7f48
Alisson David de Melo <daviddust4@hotmail.com>
2023-11-15 12:14:09 -0300

    Criando comentários dentro do projeto
    Criando comentários para tornar o código mais didático e explicativo.

--- src/main/java/criptografia/senhas/CriptografiaDeSenhas/CriptografiaDeSenhasApplication.java
+++ src/main/java/criptografia/senhas/CriptografiaDeSenhas/CriptografiaDeSenhasApplication.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/*Não executar essa classe para não precisar validar
+usuário e senha toda vez ao iniciar a aplicação.*/
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografiaDeSenhasApplication {
 
